9cbd36fffec644e3619decade97209c0e5a3aed7
Pivovarchuk Andrei <44280015+abbv0@users.noreply.github.com>
2025-05-06 04:18:23 +0300

    Update

---
+++ 1variables.py
@@ -0,0 +1,33 @@
+#1 variables 
+v_int = int(3.333)
+v_float = 1.1
+v_String = 'test spring'
+v_list = list()
+v_list = [1,'abc',3,4,5]
+v_list2 = [1,v_list]
+print(v_int)
+print(v_float)
+print(v_String)
+print(v_list)
+print(v_list2)
+
+#1 HW
+result1=v_int+1
+print('Resault1: '+ str(result1)) # addition
+result2=v_int-1
+print('Resault2: '+ str(result2)) # subtraction
+result3=v_int*1
+print('Resault3: '+ str(result3)) # multiplication
+result4=v_int/2
+print('Resault4: '+ str(result4)) # division
+result5=v_int % 2 
+print('Resault5: '+ str(result5)) # remainder after division by 2
+result6 = int(1.337) 
+print('Resault6: '+ str(result6)) # explicit type conversion
+result7_list1 = [1,4,8]
+result7_list1.append(8)
+result7_list2 = [1,3,3,7]
+result7_list1.insert(4,result7_list2)
+result7=result7_list1+result7_list2
+print('Resault7: '+ str(result7)) # - create a variable, put some values ​​in it, add more values ​​there using the .append() function, add one list inside another, add 2 different lists
+#2 Collection, dict
---
+++ 2Collection,dict.py
@@ -0,0 +1,50 @@
+#2 Collection, dict 
+list1 = [1, 2, 3, 'Hello']
+print(list1)
+print(list1[0])
+d1=dict()
+d1["first"] = 1
+d1["second"] = 12345
+print(d1.get("second"))
+d2 = {
+    'val1':1, 
+    'val2':'Hello123',
+    1:'test'
+    }
+print(d2.keys())
+print(d2.values()) 
+d3 = dict()
+d3['new'] = d2
+d3['old'] = None
+print(d3.get('new').get('val1'))
+print(type(d3))
+print(d3.items())
+
+#2 HW
+l1 = [1,2,322,4,'Hello']
+l2 = [3,'4']
+l3 = ['three','four']
+result1 = {'list1':list(set(l1)),'list2':list(set(l2)),'list3':list(set(l3))}
+print('Resault1: ',result1) # Create multiple lists. Add them together. In the new dictionary, select only unique values.
+
+d_types = {
+    'v_int': int(3.333),
+    'v_float': 1.1,
+    'v_String': 'test spring'
+    }
+d_lists={'list1':l1,'list2':l2}
+d_dicts={'dict_types':d_types,'dict_lists':d_lists}
+print('Resault2: ', d_dicts) # Create multiple dictionaries. Dictionaries with base types, dictionaries with sheets, dictionaries with dictionaries.
+result3 = d_dicts
+result3['dict_types'] = d_types.get('v_int')
+print('Resault3: ',result3.get('dict_types')) # Retrieve values ​​from dictionary via .get()
+result4 = d_dicts
+result4['additional_dict']= {'Morning':'Утро', 'Evening': 'Вечер'}
+print('Resault4: ',result4.get('additional_dict').get('Evening')) # Put one dictionary into another. Then get the values ​​from the nested dictionary
+l_key = [1,2,3,4,'Hello']
+l_values = ['one','two','three','four','World!']
+resault5 = dict(zip(l_key,l_values))
+print('Resault5: ',resault5) # Make a list with dictionary keys. Make a sheet with dictionary meanings.
+resault5[1] = 'first'
+print(resault5) # In an already created dictionary, change the value of an element
+
